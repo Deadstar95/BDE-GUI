2fb303db132ad7e3bc18ea78b3e519d69720efbe
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-07-15 07:25:02 +0200

    Leave info command for later

--- bdemgr.bat
+++ bdemgr.bat
@@ -12,8 +12,8 @@ IF %ARGUMENTCOUNT% LSS 1 GOTO HELP
 
 IF /I "%1" == "unlock" IF EXIST BDEGUI_Unlock.ps1 powershell -executionpolicy unrestricted -noprofile -nologo -file BDEGUI_Unlock.ps1 && GOTO EOF
 IF /I "%1" == "lock" IF EXIST BDEGUI_Lock.ps1 powershell -executionpolicy unrestricted -noprofile -nologo -file BDEGUI_Lock.ps1 && GOTO EOF
-IF /I "%1" == "info" IF EXIST BDEGUI_Info.ps1 powershell -executionpolicy unrestricted -noprofile -nologo -file BDEGUI_Info.ps1 && GOTO EOF
 ECHO Unknown command: %1
+ECHO.
 
 :HELP
 echo BitLocker Drive Encryption Manager - version %version%
@@ -21,7 +21,6 @@ echo.
 echo     Available commands:
 echo     - unlock -- unlocks an encrypted volume
 echo     - lock -- locks a decrypted volume
-echo     - info -- queries information about an encryptable volume
 echo.
 
 :EOF
